609e2c634307c845612e9eb9b716cc93805946a0
Mart1nSch <martin.schneider250701@gmail.com>
2026-02-28 17:12:39 -0300

    adicionei na logica quais numeros estao faltando de cada linha e coluna

--- main.py
+++ main.py
@@ -9,3 +9,32 @@ sudoku = [
     [   7,    3, None, None, None,    5, None,    4,    9],
     [   8, None, None, None, None, None, None,    2,    3],
 ]
+
+def create_reversed_sudoku(sudoku:list) -> list[list]:
+    reversed_sudoku = []
+
+    for i in range(len(sudoku)):
+        line = []
+        for j in range(len(sudoku[i])):
+            line.append(sudoku[j][i])
+        reversed_sudoku.append(line)
+    
+    return reversed_sudoku
+
+def return_missing_values(line_or_row:list) -> list:
+    missing_values = []
+
+    for i in range(1, 10):
+        if not i in line_or_row:
+            missing_values.append(i)
+    
+    return missing_values
+
+reversed_sudoku = create_reversed_sudoku(sudoku)
+
+lines_missing_values = []
+rows_missing_values = []
+for i in sudoku:
+    lines_missing_values.append(return_missing_values(i))
+for i in reversed_sudoku:
+    rows_missing_values.append(return_missing_values(i))
