56d7a6a1fe476e8054be046045c0b34d451e08a9
Mart1nSch <martin.schneider250701@gmail.com>
2026-02-28 11:21:40 -0300

    montei o sudoku inicial como matriz

---
+++ main.py
@@ -0,0 +1,11 @@
+sudoku = [
+    [None,    1, None,    4, None, None, None, None,    2],
+    [None, None, None, None,    6,    9, None, None,    4],
+    [   9, None, None, None, None, None,    6, None,    7],
+    [None,    9,    1,    6, None, None, None, None,    5],
+    [   5, None, None, None, None,    4, None,    3, None],
+    [   3,    8, None,    2, None,    1, None, None, None],
+    [   1,    2, None,    7,    9, None,    5, None, None],
+    [   7,    3, None, None, None,    5, None,    4,    9],
+    [   8, None, None, None, None, None, None,    2,    3],
+]
