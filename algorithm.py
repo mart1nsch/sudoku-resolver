14143cb3ba095a69dc4aa55e5fd1270b49b30906
Mart1nSch <martin.schneider250701@gmail.com>
2026-04-20 21:31:42 -0300

    finalizei o projeto

--- algorithm.py
+++ algorithm.py
@@ -1,78 +1,67 @@
-sudoku = [
-    [None,    1, None,    4, None, None, None, None,    2],
-    [None, None, None, None,    6,    9, None, None,    4],
-    [   9, None, None, None, None, None,    6, None,    7],
-    [None,    9,    1,    6, None, None, None, None,    5],
-    [   5, None, None, None, None,    4, None,    3, None],
-    [   3,    8, None,    2, None,    1, None, None, None],
-    [   1,    2, None,    7,    9, None,    5, None, None],
-    [   7,    3, None, None, None,    5, None,    4,    9],
-    [   8, None, None, None, None, None, None,    2,    3],
-]
+class SudokuResolver:
+    sudoku = []
 
-def find_empty() -> tuple:
-    for i in range(len(sudoku)):
-        for j in range(len(sudoku[i])):
-            if sudoku[i][j] is None:
-                return (i, j)
-
-def position_is_valid(num:int, row:int, col:int) -> bool:
+    def __init__(self, new_sudoku):
+        self.sudoku = new_sudoku
     
-    def check_area(num:int, row:int, col:int) -> bool:
-        groups_of_numbers = {
-            0: [0, 1, 2],
-            1: [0, 1, 2],
-            2: [0, 1, 2],
-            3: [3, 4, 5],
-            4: [3, 4, 5],
-            5: [3, 4, 5],
-            6: [6, 7, 8],
-            7: [6, 7, 8],
-            8: [6, 7, 8]
-        }
+    def get_sudoku(self):
+        return self.sudoku
 
-        for i in groups_of_numbers[row]:
-            for j in groups_of_numbers[col]:
-                if sudoku[i][j] == num:
+    def find_empty(self) -> tuple:
+        for i in range(len(self.sudoku)):
+            for j in range(len(self.sudoku[i])):
+                if self.sudoku[i][j] == 0:
+                    return (i, j)
+
+    def position_is_valid(self, num:int, row:int, col:int) -> bool:
+        
+        def check_area(num:int, row:int, col:int) -> bool:
+            groups_of_numbers = {
+                0: [0, 1, 2],
+                1: [0, 1, 2],
+                2: [0, 1, 2],
+                3: [3, 4, 5],
+                4: [3, 4, 5],
+                5: [3, 4, 5],
+                6: [6, 7, 8],
+                7: [6, 7, 8],
+                8: [6, 7, 8]
+            }
+
+            for i in groups_of_numbers[row]:
+                for j in groups_of_numbers[col]:
+                    if self.sudoku[i][j] == num:
+                        return False
+            return True
+
+        def check_row(num:int, row:int) -> bool:
+            for i in self.sudoku[row]:
+                if i == num:
                     return False
-        return True
+            return True
 
-    def check_row(num:int, row:int) -> bool:
-        for i in sudoku[row]:
-            if i == num:
-                return False
-        return True
+        def check_col(num:int, col:int) -> bool:
+            for i in self.sudoku:
+                if i[col] == num:
+                    return False
+            return True
 
-    def check_col(num:int, col:int) -> bool:
-        for i in sudoku:
-            if i[col] == num:
-                return False
-        return True
+        return check_area(num, row, col) and check_row(num, row) and check_col(num, col)
 
-    return check_area(num, row, col) and check_row(num, row) and check_col(num, col)
+    def solve(self) -> bool:
+        position_empty = self.find_empty()
+        if not position_empty:
+            return True
+        
+        row, col = position_empty
 
-def solve() -> bool:
-    position_empty = find_empty()
-    if not position_empty:
-        return True
-    
-    row, col = position_empty
+        for num in range(1, 10):
+            if self.position_is_valid(num, row, col):
+                self.sudoku[row][col] = num
 
-    for num in range(1, 10):
-        if position_is_valid(num, row, col):
-            sudoku[row][col] = num
-
-            if solve():
-                return True
-            
-            sudoku[row][col] = None
-    
-    return False
-
-def main():
-    solve()
-    for i in sudoku:
-        print(i)
-
-if __name__ == '__main__':
-    main()
+                if self.solve():
+                    return True
+                
+                self.sudoku[row][col] = 0
+        
+        return False
--- front.py
+++ front.py
@@ -1,1 +1,35 @@
 import streamlit as st
+import numpy as np
+from algorithm import SudokuResolver
+
+sudoku = np.zeros(81, dtype=np.int16).reshape((9, 9))
+
+st.title('Sudoku Resolver')
+st.text('Enter your sudoku (0 means No Number)')
+
+sudoku_edited = st.data_editor(sudoku, num_rows=9)
+
+if st.button('Resolve Sudoku'):
+    error = False
+    at_least_one_number = False
+
+    for i in sudoku_edited:
+        for j in i:
+            if j not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+                error = True
+                break
+            if j != 0:
+                at_least_one_number = True
+        if error:
+            break
+    
+    if not at_least_one_number:
+        error = True
+
+    if not error:
+        sudoku_resolver = SudokuResolver(sudoku_edited)
+        sudoku_resolver.solve()
+        st.text('Solution')
+        st.dataframe(sudoku_resolver.get_sudoku())
+    else:
+        st.text('Please, enter a valid Sudoku!')
